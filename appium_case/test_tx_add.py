a7579296b468bf3311780d2bb84aad0c2b29c4c9
MS-MS-MS <13439718124@163.com>
2021-09-06 18:54:32 +0800

    appium 企业微信添加联系人练习

--- appium_case/test_tx_add.py
+++ appium_case/test_tx_add.py
@@ -13,7 +13,8 @@
 
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
-
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class Test_Tx:
@@ -33,10 +34,30 @@ class Test_Tx:
         pass
 
     def test_add(self):
+        # 点击通讯录
         self.driver.find_element(MobileBy.XPATH,"//*[@text='通讯录']").click()
+        # 滑动查找到添加成员
         self.driver.find_element_by_android_uiautomator('new UiScrollable(new UiSelector().'
                                                         'scrollable(true).instance(0)).'
                                                         'scrollIntoView(new UiSelector().text("添加成员").'
+                                                        'instance(0));').click()
+        # 点击手动添加
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='手动输入添加']").click()
+        # 填写姓名
+        self.driver.find_element(MobileBy.XPATH,"//*[@class='android.widget.EditText' and @text='必填']").send_keys("0001")
+        # 账号
+        self.driver.find_elements(MobileBy.ID,"com.tencent.wework:id/b09")[1].send_keys("0001")
+        # 选择性别
+        self.driver.find_elements(MobileBy.ID,"com.tencent.wework:id/b0h")[0].click()
+        #选择女
+        WebDriverWait(self.driver,5).until(lambda x:x.find_element(MobileBy.XPATH,"//*[@text='男']"))
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='女']").click()
+        # 填写手机号
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='手机号']").send_keys("13500000001")
+        #保存
+        self.driver.find_element_by_android_uiautomator('new UiScrollable(new UiSelector().'
+                                                        'scrollable(true).instance(0)).'
+                                                        'scrollIntoView(new UiSelector().text("保存").'
                                                         'instance(0));').click()
 
 
